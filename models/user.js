8f4ba02648c628ed1c72fc0c3697788a8010a72d
tpo-krobin13 <krobin13@travelers.com>
2021-03-22 16:12:30 -0400

    Validators and custom error messages

--- models/user.js
+++ models/user.js
@@ -6,19 +6,51 @@ module.exports = (sequelize) => {
   User.init({
     name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A name is required'
+        },
+        notEmpty: {
+          msg: "Name can not be empty"
+        }
+      }
     },
     email: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'An email is required'
+        },
+        isEmail: {
+          msg: "A valid email must be provided"
+        }
+      }
     },
     birthday: {
       type: DataTypes.DATEONLY,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A birthday value is required'
+        },
+        notEmpty: {
+          msg: "Birthday can not be empty"
+        }
+      }
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A password is required'
+        },
+        notEmpty: {
+          msg: "The password can not be empty"
+        }
+      }
     }
   }, { sequelize });
 
